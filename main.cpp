dd880a8ed148131c7ec11ae5cb67e070345e4f5d
joao <joaogsg02@gmail.com>
2026-05-16 21:39:41 -0300

    main.cpp finalizado

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ void printSeparation()
 
 int main()
 {
-    //Incialização
+//Incialização
 
     // Criação de players
     Player pAna   = Player(1000, "Ana", 4730, 1);
@@ -25,16 +25,36 @@ int main()
     // Crianção do matchmaking
     Matchmaking M = Matchmaking();
 
-    // Testes
+    // Cabeçalho
     cout << "Testes de implementação:" << endl;
     printSeparation();
 
 
-    // Sistema vazio (Empty)
-    cout << "[Teste] Print do estado vazio" << endl;
+
+// Testes de sistema em estado vazio
+
+    cout << "[Teste] Print do estado vazio" << endl << endl;
     M.printWaitingPlayers();
+
+    cout << endl << endl
+         << "[Teste] Removendo e tentando formar grupos em estado vazio"
+         << endl << endl;
+    bool out1 = M.removePlayer(pAna.getId());
+    int g = -1;
+    Player* group = M.formGroup(3, 2000, &g);
+    cout << "Remoção realizada: " << out1 << " (Player não encontrado)"
+         << endl
+         << "Endereço do grupo formado: " << group << endl
+         << "Tamanho do grupo formado: g = " << g << endl; 
+
     printSeparation();
 
+
+    
+// Testes de sistema com players
+
+    cout << "[Teste] Estado do sistema após o acréscimo de 4 jogadores" 
+         << endl;
 
     // Inserindo jogadores
     M.insert(pAna);
@@ -42,65 +62,69 @@ int main()
     M.insert(pCida);
     M.insert(pDany);
 
-
-    // Novo estado do sistemna
-    cout << "[Teste] Print do sistema após o acréscimo de 4 jogadores" 
-         << endl;
     M.printWaitingPlayers();
-    printSeparation();
-
+    cout << endl << endl;
 
     // Removendo um jogador
     cout << "[Teste] Removendo um jogador" 
+         << endl
+         << "Removendo Dany (Retorno true)" << endl;
+
+    bool dany = M.removePlayer(pDany.getId());
+    M.printWaitingPlayers();
+
+    cout << endl << "Remoção realizada: " << dany << endl << endl
+         << "Removendo um player inexistente - Filipe (Retorno false)" 
          << endl;
 
-    M.removePlayer(1003);
+    bool filipe = M.removePlayer(pFilipe.getId());
     M.printWaitingPlayers();
+    cout << endl << "Remoção realizada: " << filipe << endl;
     printSeparation();
 
+// Testes de ordenação
     // Ordenando por IS
     cout << "[Teste] Ordenando por IS" 
          << endl;
 
     M.sortByScoreInsertion();
     M.printWaitingPlayers();
-    printSeparation();
 
     // Inserindo novos jogares para teste do MS:
-    cout << "Inserindo novos jogadores" 
+    cout << endl << endl
+         << "Inserindo novos jogadores" 
          << endl;
     M.insert(pFilipe); M.insert(pEnzo);
     M.printWaitingPlayers();
-    printSeparation();
-    
     
     // Ordenando por MS
-    cout << "[Teste] Ordenando por MS" 
+    cout << endl << endl
+         << "[Teste] Ordenando por MS" 
          << endl;
 
     M.sortByScoreMerge();
     M.printWaitingPlayers();
     printSeparation();
 
-
-    // Formando Grupos
+// Testes de formação de Grupos
 
     // Sucesso
     cout << "[Teste] Sucesso em Formação de um grupo" 
          << endl;
 
     int n = -1;
-    Player* group = M.formGroup(3, 2000, &n);
+    Player* group1 = M.formGroup(3, 2000, &n);
 
     cout << "Grupo: " << "|";
     for(int i = 0; i < n; i++)
     {
-        cout << group[i].getName() << "|";
+        cout << group1[i].getName() << "|";
     }
     cout << endl << "n = "<< n << endl;
 
     M.printWaitingPlayers(); cout << endl;
-    delete[] group;
+
+    if(group1 != nullptr) delete[] group1;
 
     // Fracasso
     int m = -1;
@@ -109,18 +133,40 @@ int main()
     cout << "[Teste] Fracasso em Formação de um grupo" 
          << endl;
 
-    cout << "Grupo 2:" <<"|";
+    cout << "Grupo 2:";
     for(int i = 0; i < m; i++)
     {
         cout << group2[i].getName() << "|";
     }
-    cout << endl << "m = "<< m << endl << endl;
+    cout << endl << "m = "<< m << endl;
 
     M.printWaitingPlayers();
-    delete[] group2;
+    if(group2 != nullptr) delete[] group2;
+    printSeparation();
 
-    // Recuperação de dados com getWainting Players
+// Recuperação dos dados por meio do método getWaitingPlayers;
+    cout << "[Teste] Guardando o estado do sistema com getWaitingPlayers" 
+         << endl << endl;
+    int k = -1;
+    cout << "Estado Atual:" << endl;
+    M.printWaitingPlayers();
+    Player* wp = M.getWaitingPlayers(&k);
 
+    cout << endl << "Após resetar o array de players:" << endl;
+    M.resetMatchmaking();
+    M.printWaitingPlayers();
+    
+    cout << endl << "[getWatingPlayers]" << endl;
+    for (int i = 0; i < k; i++)
+    {
+        cout << wp[i].getName() << ", ";
+    }
+    if(wp != nullptr) delete[] wp;
 
+    cout << endl << "k = " << k << endl << endl
+         << "Estado foi guardado em getWatingPlayers";
+    printSeparation();
+    
+    
     return 0;
 }
